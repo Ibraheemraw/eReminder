c3b3dfa0eff62e7f345195afff1da8f9b9d001c2
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-22 13:28:40 -0500

    rechanged the ui for the detail view for future animation purposes

--- e-Reminder/Controllers/DetailViewController.swift
+++ e-Reminder/Controllers/DetailViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailViewController: UIViewController {
+    //Outlets
+    @IBOutlet weak var contenView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.largeTitleDisplayMode = .never
-       
+        //UIColor( red: 255.0, green: 255.0, blue:255.0, alpha: 0 )
+        let myColor : UIColor = .white
+        contenView.layer.borderColor = myColor.cgColor
+        contenView.layer.borderWidth = 5
+        contenView.layer.cornerRadius = 10
+       // contenView.layer.borderColor
+        setupNavigationBar()
+    }
+    
+   private func setupNavigationBar(){
+    navigationItem.largeTitleDisplayMode = .never
+    navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "cancel", style: .plain, target: self, action: #selector(goBackToMainVC))
+    
+    }
+    @objc func goBackToMainVC(){
+        self.dismiss(animated: true, completion: nil)
     }
     
     
