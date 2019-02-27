73ebe02895042c7c743c77375302a4cd770b3a3b
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-26 19:16:36 -0500

    created the api client for the goecoding endpoint and added an appError file

--- e-Reminder/Clients/GeocodingApiClient.swift
+++ e-Reminder/Clients/GeocodingApiClient.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+final class GeocodingApiClient {
+    static func createLocations(userInput: String,  callBack: @escaping([Results]?,AppError?) -> Void){
+        //create a constant for the endpoint
+        let endPointStr = "https://maps.googleapis.com/maps/api/geocode/json?address=\(userInput)&key=\(PrivateInfoFile.GeocodingApiKey)"
+        // create a url for the endpoint
+        guard let url = URL.init(string: endPointStr) else {
+            print("bad url: \(endPointStr)")
+            return
+        }
+        //
+        let request = URLRequest.init(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error{
+                callBack(nil, AppError.networkError(error))
+            }
+            guard let response = response as? HTTPURLResponse,(200...299).contains(response.statusCode) else {
+                //callBack(nil, AppError.badStatusCode(<#T##String#>))
+                print("Bad Status Code")
+                return
+            }
+            if let data = data {
+                do {
+                    let geocodingResults = try JSONDecoder().decode(GeocodingModel.self, from: data)
+                    let results = geocodingResults.results
+                    callBack(results, nil)
+                } catch {
+                    callBack(nil, AppError.jsonDecodingError(error))
+                }
+            }
+        }
+       task.resume()
+    }
+}
---
+++ e-Reminder/PrivateInfo/PrivateInfoFile.swift
@@ -0,0 +1,13 @@
+//
+//  PrivateInfoFile.swift
+//  e-Reminder
+//
+//  Created by Ibraheem rawlinson on 2/26/19.
+//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
+//
+
+import Foundation
+
+struct PrivateInfoFile {
+    static let GeocodingApiKey = "AIzaSyCGJ29_X6M7rSwyW-Lzju93LEW2cTuOJvc"
+}
